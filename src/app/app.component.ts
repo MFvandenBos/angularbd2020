c6afde66a9b4a08339ad85359a6fbeb3208c42eb
Joris Nienkemper <joris.nienkemper@infosupport.com>
2020-05-13 11:42:34 +0200

    removed content html/css and added some examples of how you can interact from the view to the ts class and vice versa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngbd2020';
+  isNotVisible = true;
+  locaties = [
+    {stad: 'Adam', land: 'Nederland'},
+    {stad: 'Edam', land: 'Nederland'},
+    {stad: 'Obdam', land: 'Nederland'},
+  ];
+  printNaarConsole() {
+    console.log('Hello Angular!');
+  }
+  // [] initialiseer je een array in ts/js
+
 }
